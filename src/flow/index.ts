80aef48f745fec6836cca7857f0500fab9f83979
Patricio Antonio Diaz Letelier <Patricio.Diaz@aiep.cl>
2024-01-09 19:07:22 -0300

    .

--- src/flow/index.ts
+++ src/flow/index.ts
@@ -1,7 +1,7 @@
 import BotWhatsapp from '@bot-whatsapp/bot';
 import helloFlow from './hello.flow';
 import welcomeFlow from './welcome.flow';
-import paypalFlow from './paypal.flow';
+// import paypalFlow from './paypal.flow';
 import chatbotFlow from './chatbot.flow';
 import nodeFlow from './node.flow';
 
@@ -12,7 +12,7 @@ export default BotWhatsapp.createFlow(
     [
         helloFlow,
         welcomeFlow,
-        paypalFlow,
+        // paypalFlow,
         chatbotFlow,
         nodeFlow
     ]
